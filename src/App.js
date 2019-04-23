95373ae942200439b590f81ea423f46c0737f641
snehal.addweb <snehal.addweb@gamil.com>
2019-04-23 14:02:24 +0530

    reactjs-bootstrap-creative-theme 4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import Header from './component/Header';
@@ -77,31 +76,3 @@ class App extends Component {
 }
 
 export default App;
-=======
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> 97bfd9326f9a3486f4177814f5f351fa55978dfc
--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import './assets/scss/style.scss';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
-=======
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
->>>>>>> 97bfd9326f9a3486f4177814f5f351fa55978dfc
